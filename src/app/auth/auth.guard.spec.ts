d48fa65ba8138dfb682181643769b8b218fc8a92
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-06-12 15:48:42 +0200

    test: exercise route guard interfaces

--- src/app/auth/auth.guard.spec.ts
+++ src/app/auth/auth.guard.spec.ts
@@ -1,15 +1,29 @@
-import { Params, Router } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  Params,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlSegment,
+} from '@angular/router';
 
 import { AuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
 describe('AuthGuard (isolated)', () => {
+  function fakeRouterState(url: string): RouterStateSnapshot {
+    return {
+      url,
+    } as RouterStateSnapshot;
+  }
+
   beforeEach(() => {
     routerSpy = jasmine.createSpyObj<Router>('Router', ['navigate']);
     serviceStub = {};
     guard = new AuthGuard(serviceStub as AuthService, routerSpy);
   });
 
+  const dummyRoute = {} as ActivatedRouteSnapshot;
   const fakeUrls = [
     '/',
     '/admin',
@@ -25,11 +39,40 @@ describe('AuthGuard (isolated)', () => {
       serviceStub.isLoggedIn = true;
     });
 
-    fakeUrls.forEach(url => {
+    fakeUrls.forEach(fakeUrl => {
       it('grants access', () => {
-        const isAccessGranted = guard.checkLogin(url);
+        const isAccessGranted = guard.checkLogin(fakeUrl);
 
         expect(isAccessGranted).toBeTrue();
+      });
+
+      describe('when routing', () => {
+        it('can activate a route', () => {
+          const canActivate =
+            guard.canActivate(dummyRoute, fakeRouterState(fakeUrl));
+
+          expect(canActivate).toBeTrue();
+        });
+
+        it('can activate a child route', () => {
+          const canActivateChild =
+            guard.canActivateChild(dummyRoute, fakeRouterState(fakeUrl));
+
+          expect(canActivateChild).toBeTrue();
+        });
+
+        const paths = fakeUrl.split('/').filter(path => path !== '');
+
+        paths.forEach(path => {
+          it('can lazy load a feature module', () => {
+            const fakeRoute: Route = { path };
+            const fakeUrlSegment = { path } as UrlSegment;
+
+            const canLoad = guard.canLoad(fakeRoute, [fakeUrlSegment]);
+
+            expect(canLoad).toBeTrue();
+          });
+        });
       });
     });
   });
@@ -39,21 +82,21 @@ describe('AuthGuard (isolated)', () => {
       serviceStub.isLoggedIn = false;
     });
 
-    fakeUrls.forEach(url => {
+    fakeUrls.forEach(fakeUrl => {
       it('rejects access', () => {
-        const isAccessGranted = guard.checkLogin(url);
+        const isAccessGranted = guard.checkLogin(fakeUrl);
 
         expect(isAccessGranted).toBeFalse();
       });
 
       it('stores the redirect route', () => {
-        guard.checkLogin(url);
+        guard.checkLogin(fakeUrl);
 
-        expect(serviceStub.redirectUrl).toBe(url);
+        expect(serviceStub.redirectUrl).toBe(fakeUrl);
       });
 
       it('navigates to the login page', () => {
-        guard.checkLogin(url);
+        guard.checkLogin(fakeUrl);
 
         expect(routerSpy.navigate)
           .toHaveBeenCalledWith(['/login'], jasmine.any(Object));
@@ -62,7 +105,7 @@ describe('AuthGuard (isolated)', () => {
       it('adds a token to the login URL', () => {
         const expectedToken = 'anchor';
 
-        guard.checkLogin(url);
+        guard.checkLogin(fakeUrl);
 
         expect(routerSpy.navigate).toHaveBeenCalledWith(
           jasmine.any(Array),
@@ -76,13 +119,42 @@ describe('AuthGuard (isolated)', () => {
           session_id: jasmine.any(Number),
         };
 
-        guard.checkLogin(url);
+        guard.checkLogin(fakeUrl);
 
         expect(routerSpy.navigate).toHaveBeenCalledWith(
           jasmine.any(Array),
           jasmine.objectContaining({
             queryParams: expectedQueryParams,
           }));
+      });
+
+      describe('when routing', () => {
+        it('cannot activate a route', () => {
+          const canActivate =
+            guard.canActivate(dummyRoute, fakeRouterState(fakeUrl));
+
+          expect(canActivate).toBeFalse();
+        });
+
+        it('cannot activate a child route', () => {
+          const canActivateChild =
+            guard.canActivateChild(dummyRoute, fakeRouterState(fakeUrl));
+
+          expect(canActivateChild).toBeFalse();
+        });
+
+        const paths = fakeUrl.split('/').filter(path => path !== '');
+
+        paths.forEach(path => {
+          it('cannot lazy load a feature module', () => {
+            const fakeRoute: Route = { path };
+            const fakeUrlSegment = { path } as UrlSegment;
+
+            const canLoad = guard.canLoad(fakeRoute, [fakeUrlSegment]);
+
+            expect(canLoad).toBeFalse();
+          });
+        });
       });
     });
   });
