6305f639a05635dfb452b5d4a4e8aa675c2bcc97
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2020-06-24 16:14:47 +0200

    test: refactor guard integration test suite

--- src/app/auth/auth.guard.integration.spec.ts
+++ src/app/auth/auth.guard.integration.spec.ts
@@ -1,7 +1,7 @@
 import { Location } from '@angular/common';
 import { Component, Injectable, NgModule, NgZone } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { Router, RouterModule } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { of } from 'rxjs';
 
@@ -19,6 +19,113 @@ function parseUrl(url: string) {
     path,
     query,
   };
+}
+
+
+function testRouteGuard({
+  routes,
+  testUrl,
+}: {
+  routes: Routes,
+  testUrl: string,
+}) {
+  describe('AuthGuard#canActivateChild (integrated)', () => {
+    beforeEach(async () => {
+      TestBed.configureTestingModule({
+        declarations: [
+          TestLoginComponent,
+          TestRootComponent,
+          TestTargetComponent,
+        ],
+        imports: [
+          RouterTestingModule.withRoutes([
+            ...routes,
+            {
+              path: 'login',
+              component: TestLoginComponent,
+            },
+          ]),
+        ],
+        providers: [
+          { provide: AuthService, useClass: FakeAuthService },
+        ],
+      });
+
+      await TestBed.compileComponents();
+
+      rootFixture = TestBed.createComponent(TestRootComponent);
+
+      location = TestBed.inject(Location);
+      router = TestBed.inject(Router);
+      fakeService = TestBed.inject(AuthService);
+      ngZone = TestBed.inject(NgZone);
+    });
+
+    let fakeService: FakeAuthService;
+    let location: Location;
+    let ngZone: NgZone;
+    let rootFixture: ComponentFixture<TestRootComponent>;
+    let router: Router;
+
+    describe('when the user is logged in', () => {
+      beforeEach(async () => {
+        await fakeService.login().toPromise();
+      });
+
+      describe('and navigates to a guarded feature', () => {
+        beforeEach(async () => {
+          await ngZone.run(async () =>
+            canNavigate = await router.navigateByUrl(testUrl));
+        });
+
+        let canNavigate: boolean;
+
+        it('grants access', () => {
+          expect(canNavigate).toBeTrue();
+        });
+
+        it('lazy loads a feature module', () => {
+          expect(location.path()).toBe(testUrl);
+        });
+      });
+    });
+
+    describe('when the user is logged out', () => {
+      describe('and navigates to a guarded feature', () => {
+        beforeEach(async () => {
+          await ngZone.run(async () =>
+            canNavigate = await router.navigateByUrl(testUrl));
+        });
+
+        let canNavigate: boolean;
+
+        it('rejects access', () => {
+          expect(canNavigate).toBeFalse();
+        });
+
+        it('navigates to the login page', () => {
+          const { path } = parseUrl(location.path());
+          expect(path).toBe('/login');
+        });
+
+        it('stores the redirect URL', () => {
+          expect(fakeService.redirectUrl).toBe(testUrl);
+        });
+
+        it('adds a token to the login URL', () => {
+          const expectedToken = 'anchor';
+          const { fragment } = parseUrl(location.path());
+          expect(fragment).toBe(expectedToken);
+        });
+
+        it('adds a session ID to the login URL', () => {
+          const { query } = parseUrl(location.path());
+          const sessionIdPattern = /^\d+$/;
+          expect(query.get('session_id')).toMatch(sessionIdPattern);
+        });
+      });
+    });
+  });
 }
 
 @Component({
@@ -72,316 +179,34 @@ class FakeAuthService implements Partial<AuthService> {
   }
 }
 
-describe('AuthGuard#canLoad (integrated)', () => {
-  beforeEach(async () => {
-    TestBed.configureTestingModule({
-      declarations: [
-        TestLoginComponent,
-        TestRootComponent,
-        TestTargetComponent,
-      ],
-      imports: [
-        RouterTestingModule.withRoutes([
-          {
-            path: 'lazy',
-            canLoad: [AuthGuard],
-            loadChildren: () => TestFeatureModule,
-          },
-          {
-            path: 'login',
-            component: TestLoginComponent,
-          },
-        ]),
-      ],
-      providers: [
-        { provide: AuthService, useClass: FakeAuthService },
-      ],
-    });
-
-    await TestBed.compileComponents();
-
-    rootFixture = TestBed.createComponent(TestRootComponent);
-
-    location = TestBed.inject(Location);
-    router = TestBed.inject(Router);
-    fakeService = TestBed.inject(AuthService);
-    ngZone = TestBed.inject(NgZone);
-  });
-
-  let fakeService: FakeAuthService;
-  const testUrl = '/lazy';
-  let location: Location;
-  let ngZone: NgZone;
-  let rootFixture: ComponentFixture<TestRootComponent>;
-  let router: Router;
-
-  describe('when the user is logged in', () => {
-    beforeEach(async () => {
-      await fakeService.login().toPromise();
-    });
-
-    describe('and navigates to a guarded feature', () => {
-      beforeEach(async () => {
-        await ngZone.run(async () =>
-          canNavigate = await router.navigateByUrl(testUrl));
-      });
-
-      let canNavigate: boolean;
-
-      it('grants access', () => {
-        expect(canNavigate).toBeTrue();
-      });
-
-      it('lazy loads a feature module', () => {
-        expect(location.path()).toBe(testUrl);
-      });
-    });
-  });
-
-  describe('when the user is logged out', () => {
-    describe('and navigates to a guarded feature', () => {
-      beforeEach(async () => {
-        await ngZone.run(async () =>
-          canNavigate = await router.navigateByUrl(testUrl));
-      });
-
-      let canNavigate: boolean;
-
-      it('rejects access', () => {
-        expect(canNavigate).toBeFalse();
-      });
-
-      it('navigates to the login page', () => {
-        const { path } = parseUrl(location.path());
-        expect(path).toBe('/login');
-      });
-
-      it('stores the redirect URL', () => {
-        expect(fakeService.redirectUrl).toBe(testUrl);
-      });
-
-      it('adds a token to the login URL', () => {
-        const expectedToken = 'anchor';
-        const { fragment } = parseUrl(location.path());
-        expect(fragment).toBe(expectedToken);
-      });
-
-      it('adds a session ID to the login URL', () => {
-        const { query } = parseUrl(location.path());
-        const sessionIdPattern = /^\d+$/;
-        expect(query.get('session_id')).toMatch(sessionIdPattern);
-      });
-    });
-  });
+testRouteGuard({
+  routes: [{
+    path: 'lazy',
+    canLoad: [AuthGuard],
+    loadChildren: () => TestFeatureModule,
+  }],
+  testUrl: '/lazy',
 });
 
-describe('AuthGuard#canActivate (integrated)', () => {
-  beforeEach(async () => {
-    TestBed.configureTestingModule({
-      declarations: [
-        TestLoginComponent,
-        TestRootComponent,
-        TestTargetComponent,
-      ],
-      imports: [
-        RouterTestingModule.withRoutes([
-          {
-            path: 'target',
-            canActivate: [AuthGuard],
-            component: TestTargetComponent,
-          },
-          {
-            path: 'login',
-            component: TestLoginComponent,
-          },
-        ]),
-      ],
-      providers: [
-        { provide: AuthService, useClass: FakeAuthService },
-      ],
-    });
-
-    await TestBed.compileComponents();
-
-    rootFixture = TestBed.createComponent(TestRootComponent);
-
-    location = TestBed.inject(Location);
-    router = TestBed.inject(Router);
-    fakeService = TestBed.inject(AuthService);
-    ngZone = TestBed.inject(NgZone);
-  });
-
-  let fakeService: FakeAuthService;
-  const testUrl = '/target';
-  let location: Location;
-  let ngZone: NgZone;
-  let rootFixture: ComponentFixture<TestRootComponent>;
-  let router: Router;
-
-  describe('when the user is logged in', () => {
-    beforeEach(async () => {
-      await fakeService.login().toPromise();
-    });
-
-    describe('and navigates to a guarded feature', () => {
-      beforeEach(async () => {
-        await ngZone.run(async () =>
-          canNavigate = await router.navigateByUrl(testUrl));
-      });
-
-      let canNavigate: boolean;
-
-      it('grants access', () => {
-        expect(canNavigate).toBeTrue();
-      });
-
-      it('lazy loads a feature module', () => {
-        expect(location.path()).toBe(testUrl);
-      });
-    });
-  });
-
-  describe('when the user is logged out', () => {
-    describe('and navigates to a guarded feature', () => {
-      beforeEach(async () => {
-        await ngZone.run(async () =>
-          canNavigate = await router.navigateByUrl(testUrl));
-      });
-
-      let canNavigate: boolean;
-
-      it('rejects access', () => {
-        expect(canNavigate).toBeFalse();
-      });
-
-      it('navigates to the login page', () => {
-        const { path } = parseUrl(location.path());
-        expect(path).toBe('/login');
-      });
-
-      it('stores the redirect URL', () => {
-        expect(fakeService.redirectUrl).toBe(testUrl);
-      });
-
-      it('adds a token to the login URL', () => {
-        const expectedToken = 'anchor';
-        const { fragment } = parseUrl(location.path());
-        expect(fragment).toBe(expectedToken);
-      });
-
-      it('adds a session ID to the login URL', () => {
-        const { query } = parseUrl(location.path());
-        const sessionIdPattern = /^\d+$/;
-        expect(query.get('session_id')).toMatch(sessionIdPattern);
-      });
-    });
-  });
+testRouteGuard({
+  routes: [{
+    path: 'target',
+    canActivate: [AuthGuard],
+    component: TestTargetComponent,
+  }],
+  testUrl: '/target',
 });
 
-describe('AuthGuard#canActivateChild (integrated)', () => {
-  beforeEach(async () => {
-    TestBed.configureTestingModule({
-      declarations: [
-        TestLoginComponent,
-        TestRootComponent,
-        TestTargetComponent,
-      ],
-      imports: [
-        RouterTestingModule.withRoutes([
-          {
-            path: '',
-            canActivateChild: [AuthGuard],
-            children: [
-              {
-                path: 'target',
-                component: TestTargetComponent,
-              },
-            ],
-          },
-          {
-            path: 'login',
-            component: TestLoginComponent,
-          },
-        ]),
-      ],
-      providers: [
-        { provide: AuthService, useClass: FakeAuthService },
-      ],
-    });
-
-    await TestBed.compileComponents();
-
-    rootFixture = TestBed.createComponent(TestRootComponent);
-
-    location = TestBed.inject(Location);
-    router = TestBed.inject(Router);
-    fakeService = TestBed.inject(AuthService);
-    ngZone = TestBed.inject(NgZone);
-  });
-
-  let fakeService: FakeAuthService;
-  const testUrl = '/target';
-  let location: Location;
-  let ngZone: NgZone;
-  let rootFixture: ComponentFixture<TestRootComponent>;
-  let router: Router;
-
-  describe('when the user is logged in', () => {
-    beforeEach(async () => {
-      await fakeService.login().toPromise();
-    });
-
-    describe('and navigates to a guarded feature', () => {
-      beforeEach(async () => {
-        await ngZone.run(async () =>
-          canNavigate = await router.navigateByUrl(testUrl));
-      });
-
-      let canNavigate: boolean;
-
-      it('grants access', () => {
-        expect(canNavigate).toBeTrue();
-      });
-
-      it('lazy loads a feature module', () => {
-        expect(location.path()).toBe(testUrl);
-      });
-    });
-  });
-
-  describe('when the user is logged out', () => {
-    describe('and navigates to a guarded feature', () => {
-      beforeEach(async () => {
-        await ngZone.run(async () =>
-          canNavigate = await router.navigateByUrl(testUrl));
-      });
-
-      let canNavigate: boolean;
-
-      it('rejects access', () => {
-        expect(canNavigate).toBeFalse();
-      });
-
-      it('navigates to the login page', () => {
-        const { path } = parseUrl(location.path());
-        expect(path).toBe('/login');
-      });
-
-      it('stores the redirect URL', () => {
-        expect(fakeService.redirectUrl).toBe(testUrl);
-      });
-
-      it('adds a token to the login URL', () => {
-        const expectedToken = 'anchor';
-        const { fragment } = parseUrl(location.path());
-        expect(fragment).toBe(expectedToken);
-      });
-
-      it('adds a session ID to the login URL', () => {
-        const { query } = parseUrl(location.path());
-        const sessionIdPattern = /^\d+$/;
-        expect(query.get('session_id')).toMatch(sessionIdPattern);
-      });
-    });
-  });
+testRouteGuard({
+  routes: [{
+    path: '',
+    canActivateChild: [AuthGuard],
+    children: [
+      {
+        path: 'target',
+        component: TestTargetComponent,
+      },
+    ],
+  }],
+  testUrl: '/target',
 });
